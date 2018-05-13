a2c32c0cfbd7653d33029f007e21697b12ab05c8
intandinasti <37859372+intandinasti@users.noreply.github.com>
2018-05-13 20:42:46 +0700

    Add files via upload
    login

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<br><br><br>
+    <link rel="stylesheet" type="text/css" href="login.css"></link>
+    <div>
+        <center><img src="bungalogin.png"
+        height= "70" width="70"> </center><br><br>
+        
+        <form action="proses_login.php" method="post">
+
+            <input class="input" type="text" name="username" placeholder="Username"/>
+            <input class="input" type="password" name="password" placeholder="Password"/>
+            <button type="submit" class="submit">Sign in</button><br><br>
+
+        <center> <img src="hiasan.png" style: "width=70px; height=30px">
+    </div>
+</body>
+</html>
