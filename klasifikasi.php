29b6230171074fb2705286bb557cca97036ca18d
MelaRahmadani <rahmadanimela@gmail.com>
2018-05-13 23:46:35 +0700

    Edit Style

--- klasifikasi.php
+++ klasifikasi.php
@@ -9,10 +9,12 @@ $query = mysqli_query($koneksi, "SELECT * FROM klasifikasi");
 <head>
 	<title>Klasifikasi</title>
 	<link rel="stylesheet" type="text/css" href="style/gaya_klasifikasi.css">
+	<link rel="stylesheet" type="text/css" href="style/dropdown.css">
+	<link rel="stylesheet" type="text/css" href="style/list.css">
+	<link rel="stylesheet" type="text/css" href="style/form.css">
 
 </head>
 <body>
-
 	<div class="wrap">
 		<div class="header">			
            <img src="img/plantism.png" style="padding: 10px;width:480px">
@@ -20,30 +22,35 @@ $query = mysqli_query($koneksi, "SELECT * FROM klasifikasi");
 
 		<div class="menu">
 			<ul>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Help</a></li>
-                <li><a href="klasifikasi.php">Klasifikasi</a></li>	
-                <li><a href="home.php">Home</a></li>				
+				<li><a href="home.php">Home</a></li>
+				<li><a href="klasifikasi.php">Klasifikasi</a></li>
+				<li><a href="#">Contact</a></li>
+                <li><a href="#" onClick="return confirm ('Hubungi bebekgoreng@gmail.com')">Help</a></li>
 			</ul>
 		</div>
-
+`
 		<div class="badan">
-        <center>
+			<center>
             <div class="isi">
+			<h1>Form Request Artikel Plantism</h1>
+			<br>
             
             <form action= "proses_input.php" method="post">
-            <input class="input" type="text" name="nama" placeholder="nama tanaman"/>
-            <input class="input" type="text" name="kelas" placeholder="kelas tanaman"/>
-            <input type= "submit" value= "kirim">  
+            <input class="input" type="text" name="nama" placeholder="Nama Tanaman"/><br>
+            <input class="input" type="text" name="kelas" placeholder="Family"/></br>
+            <input type= "submit" value= "Request Artikel">  
 
-                    <h1> Klasifikasi Tanaman Monokotil dan Dikotil</h1>
-                    <table border ="1" align=center>
-	                    <tr>
-		                <th> id </th>
-                        <th> nama </th>
-                        <th> kelas </th>
-		                <th> Edit </th>
-	                	<th> Delete </th>
+					<br><br>
+					<h2>List Request Artikel Plantism</h2>
+					<br>
+					<center>
+					<table id="List">
+                        <tr>
+		             	   	<th> Id </th>
+                       		<th> Nama </th>
+                       		<th> Family </th>
+		               		<th> Edit </th>
+	                		<th> Delete </th>
                     	</tr>
 	
 	<?php
@@ -70,6 +77,10 @@ $query = mysqli_query($koneksi, "SELECT * FROM klasifikasi");
 		
 		<div class="clear"></div>
 		<div class="footer">
+			<br>
+			<p>Intan-Mela-Shabrina</p>
+			<br>
+		</div>
 		</div>
 	</div>
 </body>
